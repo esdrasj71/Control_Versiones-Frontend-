e4efa6d22b50bd947027522a795dcdc8183550fa
Jul10CC <julio1cayax@outlook.es>
2020-09-28 21:00:39 -0600

    Inventario hasta el combobox 21:00

--- sistema/src/app/components/inventory/inventory-form/inventory-form.component.ts
+++ sistema/src/app/components/inventory/inventory-form/inventory-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Inventory } from '../interfaces/inventory';
 
 @Component({
   selector: 'app-inventory-form',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory-form.component.css']
 })
 export class InventoryFormComponent implements OnInit {
+  inventory: Inventory = {
+    Stock: null,
+    Unit_Price: null,
+    Retail_Price: null,
+    Wholesale_Price: null,
+    Presentation_Id: null,
+    Product_Id: null,
+  };
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    //this.tipoService.getTipo().subscribe((data: Tipo) => {
+      //return this.tipo = data['data'];
+    }
 
+  saveInventory(){
+      console.log(this.inventory);
+    }
+   
 }
