ede6dd49468d9fe48423fd80325dbc5a2f6309f1
chechitajr20 <69736248+chechitajr20@users.noreply.github.com>
2020-10-01 20:23:06 -0600

    inventory

--- sistema/src/app/components/sales/bill-header-form/bill-header-form.component.ts
+++ sistema/src/app/components/sales/bill-header-form/bill-header-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Customers} from '../../customers/interfaces/customer';
 import {Products} from '../../product/interfaces/product';
+import {Inventory} from '../../inventory/interfaces/inventory';
 import { HttpClient } from '@angular/common/http';
 import {CustomersService} from '../../customers/servicios/customers.service';
 import {ProductsService} from '../../product/servicios/products.service';
+import {InventoryService} from '../../inventory/servicios/inventory.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -12,6 +14,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./bill-header-form.component.css']
 })
 export class BillHeaderFormComponent implements OnInit {
+  API_ENDPOINT = 'http://localhost:3000/'
   filtrado_clientes = '';
   clientes: any[];
   cliente: Customers[];
@@ -20,8 +23,14 @@ export class BillHeaderFormComponent implements OnInit {
   filtrado_productos = '';
   productos: any[];
   producto: Products[];
-  constructor(private customersService:CustomersService,private activatedRoute: ActivatedRoute, private httpClient: HttpClient, private productsService: ProductsService) { 
-
+  //inventario
+  inventarios: Inventory[];
+  constructor(private customersService:CustomersService,private activatedRoute: ActivatedRoute, private httpClient: HttpClient, private productsService: ProductsService, private inventoryService: InventoryService) { 
+    httpClient.get(this.API_ENDPOINT + 'inventory')
+      .subscribe((data: Inventory[]) => {
+        this.inventarios = data; //Se debe acceder al arreglo de este modo, oAngular lo reconocera como un objeto del tipo Post
+        console.log(this.inventarios);
+      });
   }
 
   ngOnInit(): void {
@@ -32,6 +41,7 @@ export class BillHeaderFormComponent implements OnInit {
     this.productsService.getProduct().subscribe((data: Products[])=>{
       return this.producto = data;
     })
+  
   }
   getCustomerId(id)
   {
@@ -48,8 +58,6 @@ export class BillHeaderFormComponent implements OnInit {
       this.productos = data;
       return this.productos = Array.of(this.productos);
     })
-  
-      
   }
 
 }
