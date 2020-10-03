aa05d1ea2afab28f2ecb6770adffcb1ffa576555
chechitajr20 <69736248+chechitajr20@users.noreply.github.com>
2020-10-02 23:01:22 -0600

    bill-header casi completo

--- sistema/src/app/components/sales/bill-header-form/bill-header-form.component.ts
+++ sistema/src/app/components/sales/bill-header-form/bill-header-form.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import {Customers} from '../../customers/interfaces/customer';
 import {Products} from '../../product/interfaces/product';
 import {Inventory} from '../../inventory/interfaces/inventory';
+import {Employee} from '../../employee/interfaces/employee';
 import { HttpClient } from '@angular/common/http';
 import {CustomersService} from '../../customers/servicios/customers.service';
 import {ProductsService} from '../../product/servicios/products.service';
 import {InventoryService} from '../../inventory/servicios/inventory.service';
+import {EmployeeService} from '../../employee/servicios/employee.service';
 import { ActivatedRoute } from '@angular/router';
 import {BillDetails} from '../interfaces/bill-detail';
-
+import {Bill_header} from '../interfaces/bill-header';
 @Component({
   selector: 'app-bill-header-form',
   templateUrl: './bill-header-form.component.html',
@@ -16,6 +18,19 @@ import {BillDetails} from '../interfaces/bill-detail';
 })
 export class BillHeaderFormComponent implements OnInit {
   values: number = 0;
+  total: number = 0;
+
+  encabezado_factura: Bill_header = {
+    Bill_header_Id: null,
+    Correlative_Number: null,
+    Serie: null,
+    Date: null,
+    Total: null,
+    Refund: null,
+    Annulment_State: null,
+    Customers_Id: null,
+    Employee_Id: null,
+  }
 
   detalle_factura: BillDetails ={
     Bill_Detail_Id: null,
@@ -40,7 +55,11 @@ export class BillHeaderFormComponent implements OnInit {
   inventario: Inventory[];
   inventarios: any[];
 
-  constructor(private customersService:CustomersService,private activatedRoute: ActivatedRoute, private httpClient: HttpClient, private productsService: ProductsService, private inventoryService: InventoryService) { 
+  //empleado
+  empleados: Employee[];
+ 
+
+  constructor(private employeeService: EmployeeService, private customersService:CustomersService,private activatedRoute: ActivatedRoute, private httpClient: HttpClient, private productsService: ProductsService, private inventoryService: InventoryService) { 
   
   }
 
@@ -55,11 +74,14 @@ export class BillHeaderFormComponent implements OnInit {
     this.inventoryService.getInventory().subscribe((data: Inventory[])=>{
       return this.inventario = data;
     })
+    this.employeeService.getEmployee().subscribe((data: Employee[])=>{
+      return this.empleados = data;
+    })
   
   }
   getCustomerId(id)
   {
-    
+    this.encabezado_factura.Customers_Id = id;
     this.customersService.getCustomerId(id).subscribe((data: Customers[])=>{
        this.clientes= data;//json
        return this.clientes=Array.of(this.clientes);
@@ -88,8 +110,26 @@ export class BillHeaderFormComponent implements OnInit {
   }
   onEnter(value: number, precio: number, datos: any){
     console.log(datos)
-    datos[0].Subtotal = value * precio;  
+    this.total -= datos[0].Subtotal;
+    datos[0].Subtotal = value * precio;
+    this.total += datos[0].Subtotal;  
+  }
+  onDelete(datos: any){
+    this.total -= datos[0].Subtotal;
+    this.nuevo = this.nuevo.filter((m)=>{
+      return m!=datos
+    })
+    console.log(this.nuevo)
   }
 
+  enviar(){
+    this.encabezado_factura.Total = this.total;
+    this.encabezado_factura
+    let venta = {
+      header:this.encabezado_factura,
+      detail: this.nuevo
+    }
+    console.log(venta);
+  }
  
 }
---
+++ sistema/src/app/components/sales/interfaces/bill-header.ts
@@ -0,0 +1,11 @@
+export interface Bill_header {
+   Bill_header_Id?: number;
+   Correlative_Number: string;
+   Serie: string;
+   Date: string;
+   Total: number;
+   Refund: number;
+   Annulment_State: number;
+   Customers_Id: number;
+   Employee_Id: number;
+}
