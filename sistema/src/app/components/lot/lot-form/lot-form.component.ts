dc1e70cf983816af76db57dbcae50154560fd2c3
Jul10CC <julio1cayax@outlook.es>
2020-10-15 18:54:19 -0600

    Cambios definitivos

--- sistema/src/app/components/lot/lot-form/lot-form.component.ts
+++ sistema/src/app/components/lot/lot-form/lot-form.component.ts
@@ -1,31 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { LotService } from '../servicios/lot.service';
 import { HttpClient } from '@angular/common/http';
 import { Lot } from '../interfaces/lot';
 import { ActivatedRoute } from '@angular/router';
 import { Products } from '../../product/interfaces/product';
 import { Router } from '@angular/router';
+import { Inventory } from '../../inventory/interfaces/inventory';
+import { InventoryService } from '../../inventory/servicios/inventory.service';
 @Component({
   selector: 'app-lot-form',
   templateUrl: './lot-form.component.html',
   styleUrls: ['./lot-form.component.css']
 })
 export class LotFormComponent implements OnInit {
-
+  @Output() Lot_Id = new EventEmitter<number>();
   lot: Lot = {
     Lot_Id: null,
     Due_Date: null,
     Product_Id: null,
-  }; //este arreglo define los campos que se van a ingresar en el formulario
+  };
+  inventory: Inventory = {
+    Stock: 0,
+    Unit_Price: 0,
+    Retail_Price: 0,
+    Wholesale_Price: 0,
+    Lot_Id: null,
+    Statuss: false,
+  };
   API_ENDPOINT = 'http://localhost:3000/';
   id: any;
   editing: boolean = false;
   postarr: Lot[];
   products: Products[];
   selectedProductId: number;
-  
 
-  constructor(private lotService: LotService, private activatedRoute: ActivatedRoute, private router: Router,private httpClient: HttpClient) {
+
+  constructor(private invetoryService: InventoryService, private lotService: LotService, private activatedRoute: ActivatedRoute, private router: Router, private httpClient: HttpClient) {
     this.id = this.activatedRoute.snapshot.params['id'];
     if (this.id) {
       this.editing = true;
@@ -52,7 +62,7 @@ export class LotFormComponent implements OnInit {
     if (this.editing) {
       this.lotService.put(this.lot).subscribe((data) => { //El unico cambioes el put
         alert('Lote actualizado');
-        console.log(data)
+
       }, (error) => {
         console.log(error);
         alert('Ocurrio un error');
@@ -63,8 +73,16 @@ export class LotFormComponent implements OnInit {
       console.log(this.lot);
       this.lotService.save(this.lot).subscribe((data) => {
         alert('Lote guardado');
-        this.router.navigate(["/lot-home"]);
-        console.log(data)
+        //this.router.navigate(["/lot-home"]);
+        this.Lot_Id.emit(data['id']);
+        this.inventory.Lot_Id = data['id'];
+        this.inventory.Statuss = false;
+        console.log(this.inventory);
+        this.invetoryService.save(this.inventory).subscribe((date) => {
+          alert('Inventario guardado');
+          console.log(date)
+        });
+
       }, (error) => {
         console.log(error);
         alert('Ocurrio un error');
@@ -76,4 +94,3 @@ export class LotFormComponent implements OnInit {
     return (product.Complete.toLocaleLowerCase().indexOf(filter) > -1);
   }
 }
-
