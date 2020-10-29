d0ba9e2ece92340199e1d000bb96d46da420773d
Andres Rivera <riveraandres1997@gmail.com>
2020-10-29 13:01:17 -0600

    pdf

--- sistema/src/app/components/inventory_reports/inventory-reports-home/inventory-reports-home.component.ts
+++ sistema/src/app/components/inventory_reports/inventory-reports-home/inventory-reports-home.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, ElementRef } from '@angular/core';
 import { InventoryReportsService } from '../servicios/inventory-reports.service';
 import { InventoryReports2Service } from '../servicios/inventory-reports2.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { InventoryReport1 } from '../interfaces/report1';
 import { InventoryReport2 } from '../interfaces/report2';
-
+import jsPDF from 'jspdf';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
 @Component({
   selector: 'app-inventory-reports-home',
   templateUrl: './inventory-reports-home.component.html',
   styleUrls: ['./inventory-reports-home.component.css']
 })
 export class InventoryReportsHomeComponent implements OnInit {
-
+  @ViewChild('pdfTable') pdfTable: ElementRef;
+  @ViewChild('pdfTable2') pdfTable2: ElementRef;
   API_ENDPOINT = 'http://localhost:3000/';
   //Variables
   reportone: InventoryReport1[];
   detail: InventoryReport1[];
   Total = 0;
   cont = 0;
-
+  Existe=0;
    //Report1
    report1: InventoryReport1 = {
     Date1: null,
@@ -35,6 +40,29 @@ export class InventoryReportsHomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  imprimir()
+{
+  const doc = new jsPDF();
+  //get table html
+  const pdfTable = this.pdfTable.nativeElement;
+  //html to pdf format
+  var html = htmlToPdfmake("<b>Nombre del sistema </b></br>"+ "<b>Fecha consultada del "+this.report1.Date1+" al "+this.report1.Date2 +"</b></br>" +pdfTable.innerHTML);
+ 
+  const documentDefinition = { content: html };
+  pdfMake.createPdf(documentDefinition).open();
+}
+
+imprimir2()
+{
+  const doc = new jsPDF();
+  //get table html
+  const pdfTable2 = this.pdfTable2.nativeElement;
+  //html to pdf format
+  var html = htmlToPdfmake("<b>Nombre del sistema </b></br>"+ "<b>Fecha consultada del "+this.report2.Date1+" al "+this.report2.Date2 +"</b></br> Detalle de la compra </br> " +pdfTable2.innerHTML);
+ 
+  const documentDefinition = { content: html };
+  pdfMake.createPdf(documentDefinition).open();
+}
    //Principal
    Reporte1() {
     if (this.report1.Date1 == null || this.report1.Date2 == null) {
@@ -43,9 +71,12 @@ export class InventoryReportsHomeComponent implements OnInit {
     else {
       this.inventoryreportService.showreport1(this.report1).subscribe((data) => {
         if (data[0] == 0) {
+          this.Existe=0;
+          this.reportone=[];
           alert('No existe inventario en ese rango de fechas');
         }
         else {
+          this.Existe=1;
           console.log(this.report1);
           this.reportone = data[0];
           console.log(this.reportone);
