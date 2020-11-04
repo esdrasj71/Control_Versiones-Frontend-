dd6556db7d9cabee282f376c0bed3a283762810c
chechitajr20 <69736248+chechitajr20@users.noreply.github.com>
2020-11-04 00:37:58 -0600

    ingresos frontend

--- sistema/src/app/components/incomes/servicios/incomes.service.ts
+++ sistema/src/app/components/incomes/servicios/incomes.service.ts
@@ -14,4 +14,24 @@ export class IncomesService {
     const headers = new HttpHeaders({'Content-Type': 'application/json', 'accesstoken': localStorage.getItem('token')});
     return this.httpClient.get(this.API_ENDPOINT + 'incomes', {headers});
   }
+
+  saveingresos(ingresos: Incomes){
+    const headers = new HttpHeaders({'Content-Type': 'application/json', 'accesstoken': localStorage.getItem('token')});
+    return this.httpClient.post(this.API_ENDPOINT + 'incomes', ingresos, {headers: headers});
+  }
+  getIncomesNuevo(){
+    const headers = new HttpHeaders({'Content-Type': 'application/json', 'accesstoken': localStorage.getItem('token')});
+    return this.httpClient.get(this.API_ENDPOINT + 'incomess', {headers});
+  }
+
+  put(ingresos) {
+    const headers = new HttpHeaders({ 'ContentType': 'application/json','accesstoken': localStorage.getItem('token') });
+    return this.httpClient.put(this.API_ENDPOINT + 'incomesactualiza/' + ingresos.Income_Date
+      , ingresos, { headers: headers });
+  }
+
+  getempresa(){
+    const headers = new HttpHeaders({'Content-Type': 'application/json', 'accesstoken': localStorage.getItem('token')});
+    return this.httpClient.get(this.API_ENDPOINT + 'empresa', {headers});
+  }
 }
